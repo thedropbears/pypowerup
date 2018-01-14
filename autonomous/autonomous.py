afaa670718dcdda919538e047fecfbafed00a4a2
michael <michael.kennedy@thedropbears.org.au>
2018-01-14 17:14:21 +1100

    Attempted to add @timedstate, probably broken

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -1,37 +1,61 @@
-from magicbot.state_machine import AutonomousStateMachine, state
+"""The autonomous controls for the robot."""
+import time
+from magicbot.state_machine import AutonomousStateMachine, state, timedstate
 from pyswervedrive.swervechassis import SwerveChassis
+
+
 
 class DriveSquareAutonoumous(AutonomousStateMachine):
     """A simple autonomuos routine designed to drive around in a square."""
 
-    chassis: SwerveChassis
+    chassis = SwerveChassis
+
     def __init__(self):
         super().__init__()
 
-    @state(first = True)
-    def DrivingForwards(self):
-        """Gives the swerve modules a positive input on the x-axis i.e. forwards."""
-        self.chassis.set_inputs(1,0,0)
-        if self.chassis.odometry >= 5:  #checks if the robot has moved 5 meters yet
-            self.next_state('DrivingLeft')  #moves the statemachine to the next state
-    @state
-    def DrivingLeft(self):
-        """Gives the swerve modules a positive input on the y-axis i.e. left"""
-        self.chassis.set_inputs(0,1,0)
+    @state(first=True)
+    def driving_forwards(self):
+        """Gives the swerve modules a positive input on the x-axis i.e.
+        forwards."""
+        self.chassis.set_inputs(1, 0, 0)
         if self.chassis.odometry >= 5:
-            self.next_state('DrivingBack')
+            # checks if the robot has moved 5 meters yet
+            self.next_state('driving_left')
+            # moves the statemachine to the next state
+
     @state
-    def DrivingBack(self):
-        """Gives the swerve modules a negative input on the x-axis i.e. backwards"""
-        self.chassis.set_inputs(-1,0,0)
+    def driving_left(self):
+        """Gives the swerve modules a positive input on the y-axis i.e.
+        left"""
+        self.chassis.set_inputs(0, 1, 0)
         if self.chassis.odometry >= 5:
-            self.next_state('DrivingRight')
+            self.next_state('driving_back')
+
     @state
-    def DrivingRight(self):
-        """Gives the swerve modules a negative input on the y-axis i.e. right"""
-        self.chassis.set_inputs(0,-1,0)
+    def driving_back(self):
+        """Gives the swerve modules a negative input on the x-axis i.e.
+        backwards"""
+        self.chassis.set_inputs(-1, 0, 0)
         if self.chassis.odometry >= 5:
-            self.next_state('Stoping')
+            self.next_state('driving_right')
+
     @state
-    def Stop(self):
-        self.chassis.set_inputs(0,0,0)
+    def driving_right(self):
+        """Gives the swerve modules a negative input on the y-axis i.e.
+        right"""
+        self.chassis.set_inputs(0, -1, 0)
+        if self.chassis.odometry >= 5:
+            self.next_state('spin_counter_clock')
+
+    @timedstate
+    def spin_counter_clock(self, duration=10):
+        """makes the robot spin WIP"""
+        while duration > 0:
+            self.chassis.set_inputs(0, 0, 1)
+            time.sleep(1)
+        self.next_state('stop')
+
+    @state
+    def stop(self):
+        """Stops the robot from moving at the end of the routine"""
+        self.chassis.set_inputs(0, 0, 0)
