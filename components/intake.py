d4169ee4a8aa197515fc2bff6c82eebade892930
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2018-01-15 20:00:50 +1100

    Add strucutre for components.

---
+++ components/intake.py
@@ -0,0 +1,26 @@
+class Intake:
+
+        def __init__(self):
+            super().__init__(self)
+
+        def setup(self):
+            """Run just after createObjects.
+            Useful if you want to run something after just once after the
+            robot code is started, that depends on injected variables.
+            """
+            pass
+
+        def on_enable(self):
+            """Run every time the robot transitions to being enabled.
+            """
+            pass
+
+        def on_disable(self):
+            """Run every time the robot transitions to being disabled.
+            """
+            pass
+
+        def execute(self):
+            """Run at the end of every control loop iteration.
+            """
+            pass
