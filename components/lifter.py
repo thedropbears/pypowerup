b0822fcb0b0c4eb7b64a564977f3cf95b06a61e1
Joel Bryla <joel@wiredlizard.com>
2018-01-17 17:13:56 +1100

    Changes function docstrings to google style, added boilerplate for moving lift

--- components/lifter.py
+++ components/lifter.py
@@ -2,7 +2,9 @@ class Lifter:
 
     def setup(self):
         """This is called after variables are injected by magicbot."""
-        pass
+        self.switch_height = None
+        self.scale_height = None
+        self.exchange_height = None
 
     def on_enable(self):
         """This is called whenever the robot transitions to being enabled."""
@@ -19,24 +21,30 @@ class Lifter:
     def move_switch(self):
         """Move the lift to the height of the switch
         """
-        pass
+        self.set_pos(self.switch_height)
 
     def move_scale(self):
         """Move the lift to the height of the scale
         """
-        pass
+        self.set_pos(self.scale_height)
 
     def move_exchange(self):
         """ Move the lift to the height of the exchange
         """
-        pass
+        self.set_pos(self.exchange_height)
     
     def set_pos(self, setpos):
         """Run pid loop to position on encoder
+
+        Args:
+            setpos (int): Encoder position to move lift to.
         """
         pass
 
     def get_pos(self):
-        """Returns encoder position
+        """Returns encoder position on lift
+
+        Returns:
+            int: The location of the lift
         """
         pass
