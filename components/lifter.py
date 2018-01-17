d00df0c70ac5059aa28e6ce457ac64a9b7093323
Joel Bryla <joel@wiredlizard.com>
2018-01-17 16:36:03 +1100

    Added boilerplate functions

--- components/lifter.py
+++ components/lifter.py
@@ -24,3 +24,28 @@ class Lifter:
             """Run at the end of every control loop iteration.
             """
             pass
+
+        def move_switch(self):
+            """Move the lift to the height of the switch
+            """
+            pass
+
+        def move_scale(self):
+            """Move the lift to the height of the scale
+            """
+            pass
+
+        def move_exchange(self):
+            """ Move the lift to the height of the exchange
+            """
+            pass
+        
+        def set_pos(self, setpos):
+            """Run pid loop to position on encoder
+            """
+            pass
+
+        def get_pos(self):
+            """Returns encoder position
+            """
+            pass
