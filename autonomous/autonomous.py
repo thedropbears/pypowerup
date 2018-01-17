8510392e4a49073284a7eb3c27f5aee4e7173104
michael <michael.kennedy@thedropbears.org.au>
2018-01-17 19:10:52 +1100

    Added subclasses of subclasses

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -1,5 +1,5 @@
-"""The autonomous controls for the robot."""
-from magicbot.state_machine import AutonomousStateMachine, state, timedstate
+"""The autonomous controls for the robot. Vision code is not yet included"""
+from magicbot.state_machine import AutonomousStateMachine, state
 from pyswervedrive.swervechassis import SwerveChassis
 
 
@@ -10,50 +10,102 @@ class OverallBase(AutonomousStateMachine):
     FMS_scale: 0  #L or R
     FMS_switch: 0  #L or R
     chassis: SwerveChassis
-    def __init__(self):
-        super().__init__()
 
-    @state
+    @state(first=True)
     def go_to_scale(self):
-        pass
+        """The robot travels to the scale"""
+        if self.FMS_scale == 'L':
+            #go to left scale
+            pass
+        if self.FMS_scale == 'R':
+            #go to right scale
+            pass
         self.next_state("deposit_cube")
 
     @state
     def deposit_cube(self):
-        pass
+        """The robot releases its cube into either the scale or switch"""
         self.next_state("intake_cube")
 
     @state
     def intake_cube(self):
-        pass
-        if:"""switch needs a cube, only happens once and if subclassed"""
-            self.next_state("go_to_switch")
-        elif: """pickup successful"""
+        """The robot drives towards where the next cube should be and then hands
+        over to vision for fine tuning"""
+        if 1 == 1:  #pickup successful
             self.next_state("go_to_scale")
 
 
-class DoubleScaleBase(OverallBase):
+#class DoubleScale(OverallBase):
+#    """A less general routine for the double-scale strategy. Still requires subclassing"""
 
-    def __init__(self):
-        super().__init__()
-        scale_first: True
-        
-    @state(first=True)
-    def go_to_scale(self):
-        pass
-        self.next_state("deposit_cube")
 
 class SwitchAndScale(OverallBase):
-    
-    def __init__(self):
-        super().__init__()
+    """A less general routine for the switch and scale strategy. Still requires subclassing"""
+    been_to_switch: False
 
-    @state(first=True)
+    @state
     def go_to_switch(self):
         """Goes to the switch, when subclassed will go to the correct side regardless of start."""
-        if FMS_switch is "L"
+        self.been_to_switch: True
+        if self.FMS_switch == 'L':
             #go to left switch
-        if FMS_switch is "R"
+            pass
+        if self.FMS_switch == 'R':
             #go to right switch
-        pass
+            pass
         self.next_state("deposit_cube")
+
+    @state
+    def intake_cube(self):
+        """The robot drives towards where the next cube should be and then hands
+        over to vision for fine tuning"""
+        if self.been_to_switch is False:  #switch needs a cube and pickup was successful
+            self.next_state("go_to_switch")
+        elif self.been_to_switch:  #The switch has a cube and pickup was successful
+            self.next_state("go_to_scale")
+
+class LeftSwitchAndScale(SwitchAndScale):
+    """The switch and scale strategy when we start on the left"""
+    MODE_NAME: 'Switch and scale - left start'
+    start_position: 0  #change to left start position
+
+    @state(first=True)
+    def go_to_scale(self):
+        if self.FMS_switch == 'L':
+            self.next_state("go_to_switch")
+        elif self.FMS_scale == 'R':
+            #go to right scale
+            pass
+        elif self.FMS_scale == 'L':
+            #go to left scale
+            pass
+        self.next_state("deposit_cube")
+
+class RightSwitchAndScale(SwitchAndScale):
+    """The switch and scale strategy when we start on the right"""
+    MODE_NAME: 'Switch and scale - right start'
+    start_position: 0  #change to right start position
+
+    @state(first=True)
+    def go_to_scale(self):
+        if self.FMS_switch == 'R':
+            self.next_state("go_to_switch")
+        elif self.FMS_scale == 'R':
+            #go to right scale
+            pass
+        elif self.FMS_scale == 'L':
+            #go to left scale
+            pass
+        self.next_state("deposit_cube")
+
+class LeftDoubleScale(OverallBase):
+    """The double switch strategy when we start on the left"""
+    MODE_NAME: 'Double scale - left start'
+    start_position: 0 #change to left start position
+    #I think i need more here but am not sure
+
+class RightDoubleScale(OverallBase):
+    """The double switch strategy when we start on the right"""
+    MODE_NAME: 'Double scale - right start'
+    start_position: 0 #change to right start position
+    #I think i need more here but am not sure
