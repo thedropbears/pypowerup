f97b6931be2013e8d322cfd665eed073369e28d6
Matthew13479 <matthew.egan@thedropbears.org.au>
2018-01-21 11:12:51 +1100

    added pneumatic functions

---
+++ automations/test_intake.py
@@ -0,0 +1,21 @@
+from magicbot import StateMachine, state, timed_state
+from components.intake import Intake
+
+
+class IntakeAutomation(StateMachine):
+    intake: Intake
+
+    @timed_state(first=True, duration=2, must_finish=True)
+    def closing(self):
+        self.arm(True)
+        self.next_state("shoot")
+
+    @timed_state(duration=2, must_finish=True)
+    def shoot(self):
+        self.arm(False)
+        self.shoot(True)
+        self.next_state("disable")
+
+    @state(must_finish=True)
+    def disable(self):
+        self.shoot(False)
