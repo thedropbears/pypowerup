d3837417da97e6e29d774269822443c0e6f5282f
Luke Sandiforth <luke.sandiforth@vlan-2723-10-17-250-70.guest.wireless.sydney.edu.au>
2018-01-29 19:47:38 +1100

    initial commit of new statemachine

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -11,7 +11,7 @@ from robot import Robot
 
 
 class OverallBase(AutonomousStateMachine):
-    """statemachine that is subclassed to all deal with all possible autonomous requirements."""
+    """statemachine designed to intelegently respond to possible situations in auto"""
 
     vision: Vision
     bno055: BNO055
@@ -23,9 +23,16 @@ class OverallBase(AutonomousStateMachine):
     # should be the closest cube
     # cubes will be listed in size order along with thier rough size and angle
 
+
     def on_enable(self):
         self.game_data_message = self.ds.getGameSpecificMessage()  # TODO test this
-        self.fails = 0
+        self.toggle_searching_closest_objective=True
+        #make y +ve or -ve depending on where we start
+        self.navigation_point=[5.6, 2.4, 0, 1]
+        self.strategy = 'DoubleScale'  # this will be set by the dashboard
+        self.start_side = 'R' # set by the dashboard
+        self.target_objective = 'R_scale'   
+
         if len(self.game_data_message) == 3:
             self.fms_scale = self.game_data_message[1]  # L or R
             self.fms_switch = self.game_data_message[0]  # L or R
@@ -37,6 +44,78 @@ class OverallBase(AutonomousStateMachine):
         self.chassis.odometry_y = 0
         super().on_enable()
 
+    @state(first=true)
+    def setup(self)
+        if switch_first==true:
+            self.next_state("go_to_switch")
+            #change switch postion one off
+        else:
+            self.next_state("navigation_point")
+
+    def invert_co_ordinates(co_ordinate)
+        for i in co_ordinate:
+            co_ordinate[1] *= -1
+        return co_ordinate
+
+    def close_to_objective(objective)
+        if self.start_side == self.fms_switch and objective == 'switch':
+            return True
+        else:
+            return False
+        if self.start_side == self.fms_scale and objective == 'scale':
+            return True
+        else:
+            return False
+
+    @state
+    def navigation_point(self, initial_call):
+        if initial_call:
+            #seraching for objective
+            if toggle_searching_for_objective:
+                if close_to_objective(self.target_objective):
+                    # at correct nav point
+                    self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0],self.navigation_point])
+                else:
+                    # go to other navigation point
+                    self.navpoint = invert_co_ordinates(self.navpoint)
+                    self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0],self.navigation_point])
+
+
+                if self.target_objective == 'switch':
+                    self.next_state('go_to_switch')
+                else:
+                    self.next_state('go_to_scale')
+            else:
+                #serach for cube , nav  point close to us
+                self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0],self.navigation_point])
+                self.next_state("go_to_cube")
+            #self.toggle_searching_closest_objective=not(toggle_searching_closest_objective)
+            
+    @state
+    def go_to_cube(self, initial_call):
+        """The robot drives towards where the next cube should be"""
+        if initial_call:
+            angle = self.bno055.getAngle()
+            self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0]])
+        if not self.motion.enabled:
+
+            self.next_state_now("deposit_cube")        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
     @state(first=True)
     def go_to_scale(self):
         """The robot travels to the scale"""
@@ -55,11 +134,17 @@ class OverallBase(AutonomousStateMachine):
         self.next_state("go_to_cube")
 
     @state
-    def go_to_cube(self):  # this will need to be overridden in the subclasses
+    def go_to_cube(self, initial_call):
         """The robot drives towards where the next cube should be"""
-        # go to closest_cube
-        if 1 == 1:  # Gets to cube position (odometry)
-            self.next_state("search_for_cube")
+        if initial_call:
+            angle = self.bno055.getAngle()
+            self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0],
+                                       [2.5, 0, math.pi/2, 1],
+                                       [2.5, 1, math.pi/2, 0]])
+        if not self.motion.enabled:
+            # print("going to 'search_for_cube'")
+            # self.next_state("search_for_cube")
+            self.next_state_now("deposit_cube")
 
     @state
     def search_for_cube(self):
@@ -107,7 +192,7 @@ class OverallBase(AutonomousStateMachine):
     def dead_reckon(self):
         """The robot tries to find the cube without the assistance of vision.
         likely to fail used as a last resort if vision has failed multiple times."""
-        # go to cube and run intake
+        # go to cube and run intake '''
 
 
 class VisionTest(OverallBase):
@@ -118,12 +203,6 @@ class VisionTest(OverallBase):
     @state
     def go_to_cube(self, initial_call):
         """The robot drives towards where the next cube should be"""
-        if self.fms_scale == 'L':
-            # go to left scale
-            print('Scale is left')
-        if self.fms_scale == 'R':
-            # go to right scale
-            print('Scale is right')
         if initial_call:
             angle = self.bno055.getAngle()
             self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0],
@@ -154,140 +233,3 @@ class VisionTest(OverallBase):
             print("================= At scale ======================")
             self.next_state("go_to_cube")
 
-
-class SwitchAndScale(OverallBase):
-    """A less general routine for the switch and scale strategy. Still requires subclassing"""
-
-    def __init__(self):
-        self.been_to_switch = False
-        super().__init__()
-
-    @state
-    def go_to_switch(self):
-        """Goes to the switch, when subclassed will go to the correct side regardless of start."""
-        self.been_to_switch: True
-        if self.fms_switch == 'L':
-            # go to left switch
-            pass
-        if self.fms_switch == 'R':
-            # go to right switch
-            pass
-        self.next_state("deposit_cube")
-
-    @state
-    def intake_cube(self):
-        """Attempts to intake the cube"""
-        # Run intake
-        if self.been_to_switch:  # cube inside intake system (current spikes)
-            self.next_state("go_to_scale")
-        elif self.been_to_switch is False:  # switch needs a cube and pickup was successful
-            self.next_state("go_to_switch")
-        if 1 == 1:  # After completing the intake cycle there is no cube
-            self.next_state("search_for_cube")
-
-
-class LeftSwitchAndScale(SwitchAndScale):
-    """The switch and scale strategy when we start on the left"""
-    MODE_NAME: 'Switch and scale - left start'
-    start_position = 3  # change to left start position
-
-    @state(first=True)
-    def go_to_scale(self):
-        if self.fms_switch == 'L':
-            self.next_state("go_to_switch")
-        elif self.fms_scale == 'R':
-            # go to right scale
-            pass
-        elif self.fms_scale == 'L':
-            # go to left scale
-            pass
-        self.next_state("deposit_cube")
-
-
-class RightSwitchAndScale(SwitchAndScale):
-    """The switch and scale strategy when we start on the right"""
-    MODE_NAME: 'Switch and scale - right start'
-    start_position = -3  # change to right start position
-    coordinates = [[[4.24, -2.0, 1.60, 1],
-                    [5.5, -2.75, 2, 1],
-                    [5.25, -1.91, 2, 1],
-                    [7.5, -1.87, 0, 1]],  # SameSwitchSameScale
-                   [[4.29, -1.91, 1.45, 1],
-                    [5.5, -2.5, 1.8, 1],
-                    [5.08, 1.90, 0, 1],
-                    [7.07, 1.90, 0, 1]],  # SameSwitchCrossScale
-                   [[7.5, -1.87, 0, 1],
-                    [5.5, -2, 2, 1],
-                    [6, 2.1, 4.71239, 1],
-                    [4.2, 2.1, 4.71239, 1]],  # CrossSwitchSameScale
-                   [[5, -3, 0, 1],
-                    [6, 2, 0, 1],
-                    [7.5, 1.8, 0, 1],
-                    [5.5, 2, 3.92699, 1],
-                    [5.25, 1.5, 3.14159, 1]]]  # CrossSwitchCrossScale
-
-    @state(first=True)
-    def go_to_scale(self):
-        if self.fms_switch == 'R':
-            self.next_state("go_to_switch")
-        elif self.fms_scale == 'R':
-            # go to right scale
-            pass
-        elif self.fms_scale == 'L':
-            # go to left scale
-            pass
-        self.next_state("deposit_cube")
-
-
-class LeftDoubleScale(OverallBase):
-    """The double switch strategy when we start on the left"""
-    MODE_NAME: 'Double scale - left start'
-    coordinates = [[[8, 2.25, 1.5708, 1],
-                    [6.10, 2.50, 2.35619, 1],
-                    [5.25, 1.80, 2.35619, 1],
-                    [7.50, 2, 0, 1]],  # coordinates for same scale
-                   [[5, 3, 0, 1],
-                    [6, -2, 0, 1],
-                    [7.5, -1.8, 0, 1],
-                    [5.5, -2, 3.92699, 1],
-                    [7.5, -2, 0, 1]]]  # coordinates for cross scale
-
-    @state(first=True)
-    def go_to_scale(self):
-        if self.fms_scale == 'R':
-            self.coordinates = self.coordinates[1]
-            self.motion.set_waypoints = self.coordinates[:2]
-            # go to right scale
-        elif self.fms_scale == 'L':
-            self.coordinates = self.coordinates[0]
-            self.motion.set_waypoints = np.array(self.coordinates[0])
-            # go to left scale
-        self.next_state("deposit_cube")
-
-
-class RightDoubleScale(OverallBase):
-    """The double switch strategy when we start on the right"""
-    MODE_NAME: 'Double scale - right start'
-    coordinates = [[[8, -2.25, 1.5708, 1],
-                    [6.10, -2.50, 2.35619, 1],
-                    [5.25, -1.80, 2.35619, 1],
-                    [7.50, -2, 0, 1]],  # coordinates for same scale
-                   [[5, -3, 0, 1],
-                    [6, 2, 0, 1],
-                    [7.5, 1.8, 0, 1],
-                    [5.5, 2, 3.92699, 1],
-                    [7.5, 2, 0, 1]]]  # coordinates for cross scale
-
-    @state(first=True)
-    def go_to_scale(self):
-        if self.fms_scale == 'R':
-            self.coordinates = self.coordinates[0]
-            self.motion.set_waypoints = np.array(self.coordinates[0])
-            # go to right scale
-        elif self.fms_scale == 'L':
-            self.coordinates = self.coordinates[1]
-            self.motion.set_waypoints = np.array(self.coordinates[0])
-            self.motion.set_waypoints = np.array(self.coordinates[1])
-            self.motion.set_waypoints = np.array(self.coordinates[2])
-            # go to left scale
-        self.next_state("deposit_cube")
