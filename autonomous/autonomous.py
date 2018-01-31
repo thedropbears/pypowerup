9e6008c23eb430fc276fe8a2c9e2bd00daad1914
Luke Sandiforth <luke.sandiforth@vlan-2723-10-17-251-86.guest.wireless.sydney.edu.au>
2018-01-31 18:56:21 +1100

    Added Target Objective function

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -29,7 +29,7 @@ class OverallBase(AutonomousStateMachine):
         self.toggle_searching_closest_objective=True
         #make y +ve or -ve depending on where we start
         self.navigation_point=[5.6, 2.4, 0, 1]
-        self.strategy = 'DoubleScale'  # this will be set by the dashboard
+        self.strategy = 'double_scale'  # this will be set by the dashboard
         self.start_side = 'R' # set by the dashboard
         self.target_objective = 'R_scale'   
 
@@ -52,12 +52,12 @@ class OverallBase(AutonomousStateMachine):
         else:
             self.next_state("navigation_point")
 
-    def invert_co_ordinates(co_ordinate)
+    def invert_co_ordinates(co_ordinate):
         for i in co_ordinate:
             co_ordinate[1] *= -1
         return co_ordinate
 
-    def close_to_objective(objective)
+    def close_to_objective(objective):
         if self.start_side == self.fms_switch and objective == 'switch':
             return True
         else:
@@ -67,7 +67,35 @@ class OverallBase(AutonomousStateMachine):
         else:
             return False
 
-    @state
+    def target_objective():
+        """A dictionary function that gives an output on the order of objectives based on inputs.
+        The inputs are designated on the left in a tuple in the order of robot start side, strategy, switch side
+        scale side. The outputs are given on the right in the order which the robot needs to go to"""
+        objective_calculation = {
+            # Double scale with right start
+            ('R','double_scale','L','L'): ('cross_l_scale', 'same_l_scale'),
+            ('R','double_scale','R','L'): ('cross_l_scale', 'same_l_scale'),
+            ('R','double_scale','R','R'): ('same_r_scale', 'same_r_scale'),
+            ('R','double_scale','L','R'): ('same_r_scale', 'same_r_scale'),
+            # Double scale with left start
+            ('L','double_scale','L','L'): ('same_l_scale', 'same_l_scale'),
+            ('L','double_scale','R','L'): ('same_l_scale', 'same_l_scale'),
+            ('L','double_scale','R','R'): ('cross_r_scale', 'same_r_scale'),
+            ('L','double_scale','L','R'): ('cross_r_scale', 'same_r_scale'),
+            # Switch and scale with right start
+            ('R','switch_and_scale','L','R'): ('cross_l_scale', 'same_r_scale'),
+            ('R','switch_and_scale','L','L'): ('cross_l_scale', 'same_l_switch'),
+            ('R','switch_and_scale','R','L'): ('same_r_switch', 'cross_l_scale'),
+            ('R','switch_and_scale','R','R'): ('same_r_switch', 'same_r_scale'),
+            # Switch and scale with left start
+            ('L','switch_and_scale','R','L'): ('same_l_scale', 'cross_r_switch'),
+            ('L','switch_and_scale','R','R'): ('cross_r_scale', 'same_r_switch'),
+            ('L','switch_and_scale','L','R'): ('same_l_switch', 'cross_r_scale'),
+            ('L','switch_and_scale','L','L'): ('same_l_switch', 'same_l_scale')
+        }
+        return objective_calculation[(self.start_side, self.strategy, self.fms_switch, self.fms_scale)]
+
+    @state(first=True)
     def navigation_point(self, initial_call):
         if initial_call:
             #seraching for objective
@@ -99,60 +127,12 @@ class OverallBase(AutonomousStateMachine):
             self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0]])
         if not self.motion.enabled:
 
-            self.next_state_now("deposit_cube")        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-    @state(first=True)
-    def go_to_scale(self):
-        """The robot travels to the scale"""
-        if self.fms_scale == 'L':
-            # go to left scale
-            pass
-        if self.fms_scale == 'R':
-            # go to right scale
-            pass
-        self.next_state("deposit_cube")
+            self.next_state_now("deposit_cube") 
 
     @state
-    def deposit_cube(self):
-        """The robot releases its cube into either the scale or switch"""
-        # Release cube
-        self.next_state("go_to_cube")
-
-    @state
-    def go_to_cube(self, initial_call):
-        """The robot drives towards where the next cube should be"""
-        if initial_call:
-            angle = self.bno055.getAngle()
-            self.motion.set_waypoints([[self.chassis.odometry_x, self.chassis.odometry_y, angle, 0],
-                                       [2.5, 0, math.pi/2, 1],
-                                       [2.5, 1, math.pi/2, 0]])
-        if not self.motion.enabled:
-            # print("going to 'search_for_cube'")
-            # self.next_state("search_for_cube")
-            self.next_state_now("deposit_cube")
-
-    @state
-    def search_for_cube(self):
-        """The robot attemppts to find a cube within the frame of the camera"""
-        if self.vision.largest_cube() is None:
-            self.chassis.set_inputs(0, 0, 1)
-        else:
-            self.next_state("turn_and_go_to_cube")
+    def deposit_cube(self):             
+    # This is for depositing the cube to either the Scale or The switch 
+    self.next_state("navigation_point")  
 
     @state
     def turn_and_go_to_cube(self):
@@ -166,34 +146,45 @@ class OverallBase(AutonomousStateMachine):
             self.next_state_now("search_for_cube")  # tempoary for testing
             return
             print("========searching for cube========")
-        if not self.cube_switch.get():
-            self.next_state_now("go_to_scale")
-            print('===========Going to scale===========')
-            return
         absolute_cube_direction = angle + vision_angle
         new_heading = angle + 0.2 * vision_angle
         self.chassis.field_oriented = True
         self.chassis.set_velocity_heading(math.cos(absolute_cube_direction),
                                           math.sin(absolute_cube_direction),
                                           new_heading)
-        # TODO: implement state transition
-        # self.next_state("intake_cube")
+        self.next_state("intake_cube")
 
     @state
-    def intake_cube(self):
-        """Attempts to intake the cube"""
-        # Run intake
-        if 1 == 1:  # cube inside intake system (current spikes)
-            self.next_state("go_to_scale")
-        elif 2 == 2:  # After completing the intake cycle there is no cube
-            self.next_state("search_for_cube")
+    def search_for_cube(self):
+        """The robot attemppts to find a cube within the frame of the camera"""
+        if self.vision.largest_cube() is None:
+            self.chassis.set_inputs(0, 0, 1)
+        else:
+            self.next_state("turn_and_go_to_cube")
 
     @state
-    def dead_reckon(self):
-        """The robot tries to find the cube without the assistance of vision.
-        likely to fail used as a last resort if vision has failed multiple times."""
-        # go to cube and run intake '''
+    def go_to_switch(self):
+        """The robot travels to the switch"""
+        if self.fms_switch == 'L':
+            # go to left switch
+            pass
+        if self.fms_switch == 'R':
+            # go to right switch
+            pass
+        self.next_state("deposit_cube")
 
+    @state
+    def go_to_scale(self):
+        """The robot travels to the scale"""
+        if self.fms_scale == 'L':
+            # go to left scale
+            pass
+        if self.fms_scale == 'R':
+            # go to right scale
+            pass
+        self.next_state("deposit_cube")
+
+    @state
 
 class VisionTest(OverallBase):
     """To test the vision system"""
