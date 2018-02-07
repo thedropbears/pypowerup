269b17b7df7677410f6bd7709a9cc80fbd086bad
Arthur Allshire <arthur@allshire.org>
2018-02-07 20:07:52 +1100

    Vision: update for new regime

--- components/vision.py
+++ components/vision.py
@@ -1,18 +1,19 @@
 import time
-import numpy as np
+
 from networktables import NetworkTables
 
 
 class Vision:
+    CAMERA_HEIGHT = 0.42  # m
 
-    def setup(self):
+    def __init__(self):
         """This is called after variables are injected by magicbot."""
-        self.nt = NetworkTables
-        self.sd = self.nt.getTable("/vision")
-        self.entry = self.sd.getEntry("info")
-        self.entry.addListener(self.new_value, self.nt.NotifyFlags.UPDATE)
+        self.nt = NetworkTables.getTable("/vision")
+        self.entry = self.nt.getEntry("info")
+        self.entry.addListener(self.new_value, NetworkTables.NotifyFlags.NEW | NetworkTables.NotifyFlags.UPDATE)
 
-        self.vision = []
+        self.time = 0
+        self.data = []
 
     def on_enable(self):
         """This is called whenever the robot transitions to being enabled."""
@@ -27,30 +28,10 @@ class Vision:
         pass
 
     def new_value(self, entry, key, value, param):
-        start_time = time.time()
-        if key == "/vision/info":
-            self.unpack_info(value, start_time)
-
-    def format_list(self, lst, n):
-        out = [lst[i:i+n] for i in range(0, len(lst), n)]
-        t = out[-1][0]
-        out.pop(-1)
-        out.append(t)
-        return out
-
-    def unpack_info(self, info, start_time):
-        if len(info) <= 1:
-            self.vision = [None]
-            return
-
-        info = np.array(info)
-        info = self.format_list(info, 3)
-        total_time = (time.time() - start_time) + float(info[-1])
-        info[-1] = total_time
-
-        self.vision = info
+        self.time = time.monotonic()
+        self.data = value
 
     def largest_cube(self):
-        if len(self.vision) < 2:
+        if len(self.data) < 2:
             return None
-        return self.vision[0][0]
+        return self.data[0]
