0bd2f94ae7b0fa169ef076747e16c455a58da30b
David Vo <auscompgeek@users.noreply.github.com>
2018-07-21 12:43:23 +1000

    vector_pursuit: Clean up

--- utilities/vector_pursuit.py
+++ utilities/vector_pursuit.py
@@ -1,4 +1,5 @@
 import math
+
 import numpy as np
 from networktables.util import ntproperty
 
@@ -17,8 +18,8 @@ class VectorPursuit:
                 orientation, only speed and position.
         """
         self.waypoints = waypoints
-        self.waypoints_xy = np.array([[waypoint[0], waypoint[1]] for waypoint in self.waypoints])
-        self.segment_idx = None
+        self.waypoints_xy = np.array([waypoint[:2] for waypoint in self.waypoints])
+        self.segment_idx = -1
         self.increment_segment()
 
     def set_motion_params(self, top_speed, top_accel, top_decel):
@@ -26,20 +27,17 @@ class VectorPursuit:
 
         Args:
             cruise_vel: Cruise velocity of the drivebase (top speed the
-                controller will set). Units m/s.
-            top_accel: Maximum acceleration the controller will set. Units
-                m/s/s.
-            top_decel: Maximum deceleration the controller will set. Must be
-                less than 0. Units m/s/s.
+                        controller will set), in m/s.
+            top_accel: Maximum acceleration the controller will set, in m/s/s.
+            top_decel: Maximum deceleration the controller will set, in m/s/s.
+                       Must be negative.
         """
         self.top_speed = top_speed
         self.top_accel = top_accel
         self.top_decel = top_decel
 
-    def increment_segment(self, position=None, segment=None, start_speed=None, start_segment_tm=None):
-        if self.segment_idx is None:
-            self.segment_idx = 0
-        elif segment is not None:
+    def increment_segment(self, position=None, segment: int = None, start_speed=None, start_segment_tm=None):
+        if segment is not None:
             if segment == self.segment_idx:
                 return
             self.segment_idx = segment
@@ -50,8 +48,8 @@ class VectorPursuit:
         # if start_speed is None:
         #     start_speed = self.waypoints[self.segment_idx][3]
 
-        if position is None:
-            position = self.waypoints_xy[self.segment_idx]
+        # if position is None:
+        #     position = self.waypoints_xy[self.segment_idx]
         # end_speed = self.waypoints[self.segment_idx+1][3]
         # self.start_to_end = np.linalg.norm(position - self.waypoints_xy[self.segment_idx+1])
         # self.speed_function, end_tm = generate_trapezoidal_function(
@@ -65,18 +63,19 @@ class VectorPursuit:
 
     def get_output(self, position: np.ndarray, speed: float, current_tm=None):
         """Compute the angle to move the robot in to converge with waypoints.
+
         Args:
-            position: Robot's position as an [x, y] numpy array. Units m.
-            speed: Robot's speed as a scalar value (i.e. the norm of it's x, y
-                velocity), must be >0. Units of m/s.
+            position: Robot's position as an [x, y] numpy array, in metres.
+            speed: Robot's speed as a scalar value (i.e. the norm of its x, y
+                   velocity), in m/s. Must be positive.
+
         Returns:
             float: Direction of motion to command the robot at. Units radians
-                clockwise from the positive x axis.
-            float: Speed of motion to command the robot at. Units of m/s.
+                   clockwise from the positive x axis.
             bool: Whether we moved to the next waypoint or not.
-            bool: Wherther we finished executing the entire set trajectory.
-        """
+            bool: Whether we finished executing the entire set trajectory.
 
+        """
         # check if at edge of segment
         displacement = position - self.waypoints_xy[self.segment_idx]
         scale = displacement.dot(self.segment) / self.segment.dot(self.segment)
@@ -108,8 +107,8 @@ class VectorPursuit:
             # take away from the remaining look ahead the amount we have travelled
             # along the segment
             look_ahead_remaining -= np.linalg.norm(segment_end - projected_point)
-            if (look_ahead_waypoint == len(self.waypoints)-2 or
-                    look_ahead_remaining <= 0):
+            if (look_ahead_waypoint == len(self.waypoints) - 2
+               or look_ahead_remaining <= 0):
                 break
 
             # projected point is now the start of the next segment
@@ -118,9 +117,7 @@ class VectorPursuit:
 
         before_increment = self.segment_idx
         self.increment_segment(position, look_ahead_waypoint, start_speed=speed, start_segment_tm=current_tm)
-        next_seg = False
-        if not before_increment == self.segment_idx:
-            next_seg = True
+        next_seg = before_increment != self.segment_idx
 
         # calculate angle of look ahead from oreintation
         new_x, new_y = look_ahead_point - position
